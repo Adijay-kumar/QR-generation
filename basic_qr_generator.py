2878394cf15e67b4a72b9a16be16998511a707cb
Adijay Kumar Kashyap <adijaykumarkashyap@gmail.com>
2026-01-12 13:54:46 +0530

    Refactor input handling and improve user prompts

--- basic_qr_generator.py
+++ basic_qr_generator.py
@@ -1,15 +1,34 @@
+"""
+File Name: basic_qr_generator.py
+
+WHEN TO USE THIS FILE:
+---------------------
+Run this file when you only need QUICK QR generation
+and DO NOT care about saving the count between runs.
+
+Each time the program is restarted, numbering will begin from 0.
+
+This is useful for:
+- Learning while loops
+- Understanding counters
+- Small demos
+- One-time QR creation
+"""
+
 import qrcode
 
+# Counter starts fresh every time the program runs
 count = 0
 
 while True:
+    repo_url = input("Enter the github repo link (type 'done' to stop): ")
 
-    repo_url = input("Enter the github repo link : ")
-    if repo_url == "done":
+    if repo_url.lower() == "done":
         break
-    else:
-        qr = qrcode.make(repo_url)
-        file_name = f"project_qr_{count}.png"
-        qr.save(file_name)
-        print(file_name , "created succesfully")
-        count += 1
+
+    qr = qrcode.make(repo_url)
+    file_name = f"project_qr_{count}.png"
+    qr.save(file_name)
+
+    print(file_name, "created successfully")
+    count += 1
